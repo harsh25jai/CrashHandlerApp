734931a839af32fb529ac54cb090713ac5bc75c3
Harsh Jaiswal <harsh25jai@gmail.com>
2026-02-16 18:28:21 +0530

    Android Step 2.5: Wire Up the Application

--- android/app/src/main/java/com/crashhandlerapp/MainApplication.kt
+++ android/app/src/main/java/com/crashhandlerapp/MainApplication.kt
@@ -6,6 +6,9 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+// Adding NativeCrashHandler
+import com.nativecrashhandler.CrashHandlerPackage
+import com.nativecrashhandler.CrashHandler
 
 class MainApplication : Application(), ReactApplication {
 
@@ -16,12 +19,43 @@ class MainApplication : Application(), ReactApplication {
         PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // add(MyReactNativePackage())
+          add(CrashHandlerPackage())
         },
     )
   }
 
+  private var currentActivity: android.app.Activity? = null
+  private val crashHandler = CrashHandler()
+
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
+
+    // Initialize global crash handler once
+    Thread.setDefaultUncaughtExceptionHandler(crashHandler)
+    
+    registerActivityLifecycleCallbacks(object : android.app.Application.ActivityLifecycleCallbacks {
+      override fun onActivityCreated(activity: android.app.Activity, savedInstanceState: android.os.Bundle?) {
+        currentActivity = activity
+        crashHandler.updateActivity(activity)
+      }
+      override fun onActivityStarted(activity: android.app.Activity) { 
+        currentActivity = activity
+        crashHandler.updateActivity(activity)
+      }
+      override fun onActivityResumed(activity: android.app.Activity) { 
+        currentActivity = activity
+        crashHandler.updateActivity(activity)
+      }
+      override fun onActivityPaused(activity: android.app.Activity) {}
+      override fun onActivityStopped(activity: android.app.Activity) {}
+      override fun onActivitySaveInstanceState(activity: android.app.Activity, outState: android.os.Bundle) {}
+      override fun onActivityDestroyed(activity: android.app.Activity) {
+        if (currentActivity == activity) {
+            currentActivity = null
+            crashHandler.updateActivity(null)
+        }
+      }
+    })
   }
 }
