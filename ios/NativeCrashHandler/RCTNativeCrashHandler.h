9c2aecd4d8769e9bf152cb4f0ea8f74797d13e17
Harsh Jaiswal <harsh25jai@gmail.com>
2026-02-17 16:41:50 +0530

    iOS Step 2.7: Implement the Objective-C++ Header

--- ios/NativeCrashHandler/RCTNativeCrashHandler.h
+++ ios/NativeCrashHandler/RCTNativeCrashHandler.h
@@ -6,10 +6,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <NativeCrashHandler/NativeCrashHandler.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RCTNativeCrashHandler : NSObject
+@interface RCTNativeCrashHandler : NSObject <NativeCrashHandlerSpec>
 
 @end
 
